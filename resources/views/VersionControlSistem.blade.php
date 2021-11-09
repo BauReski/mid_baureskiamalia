50685d788b064b83d8323a21fb0b000e0c5b26e1
BauReski <baureskiamalia0308@gmail.com>
2021-11-09 12:25:39 +0800

    BISMILLAH LULUS

---
+++ resources/views/VersionControlSistem.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+Version control adalah sebuah sistem yang merekam perubahan-perubahan dari sebuah berkas atau sekumpulan berkas dari waktu ke waktu sehingga Anda dapat menilik kembali versi khusus suatu saat nanti.
+Macam-macam version control :
+1. Bazaar
+2. GitHub
+3. Git
+4. Mercuriel
+5. CVS
+</body>
+</html>
